2524c33d989ea26c879d928aecc64df318e0ab38
Matteo Stanislao Giarrusso <matteo.giarrusso@demetrix.it>
2024-06-04 17:10:42 +0200

    github actions: removed unnecessary files, fixed test and build stages

--- src/test/java/com/glaciation/TradeOffService/TradeOffServiceApplicationTests.java
+++ src/test/java/com/glaciation/TradeOffService/TradeOffServiceApplicationTests.java
@@ -1,13 +1,18 @@
 package com.glaciation.TradeOffService;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TradeOffServiceApplicationTests {
 
+	private final Logger logger = LoggerFactory.getLogger(TradeOffServiceApplicationTests.class);
+
 	@Test
 	void contextLoads() {
+		logger.info("Dependencies are set up, configuration is correct and context loaded successfully.");
 	}
 
 }
