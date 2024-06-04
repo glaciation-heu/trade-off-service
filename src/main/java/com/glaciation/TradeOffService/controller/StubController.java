84fcf35bb9bc58ff26724b3aebbaa5aa3e5e1f94
Matteo Stanislao Giarrusso <matteo.giarrusso@demetrix.it>
2024-06-04 17:53:06 +0200

    improved open api documentation, fixed "github release" step

--- src/main/java/com/glaciation/TradeOffService/controller/StubController.java
+++ src/main/java/com/glaciation/TradeOffService/controller/StubController.java
@@ -1,6 +1,14 @@
 package com.glaciation.TradeOffService.controller;
 
+import com.glaciation.TradeOffService.model.Node;
+import com.glaciation.TradeOffService.model.Workload;
 import com.glaciation.TradeOffService.service.StubService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
@@ -16,22 +24,60 @@ public class StubController {
     StubService stubService;
 
     @GetMapping("/workloads")
-    public ResponseEntity<?> getWorkloads(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") Date startTime, @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") Date endTime) {
+    @Operation(description = "Get data and metadata about all workloads")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Workloads list", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Workload.class))),
+            @ApiResponse(responseCode = "400", description = "Malformed request", content = @Content),
+            @ApiResponse(responseCode = "500", description = "An error occurred on the server side", content = @Content)
+    })
+    public ResponseEntity<?> getWorkloads(
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") @Parameter(description = "Date indicating the beginning of the aggregation") Date startTime,
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") @Parameter(description = "Date indicating the end of the aggregation") Date endTime
+    ) {
         return this.stubService.getWorkloads(startTime, endTime);
     }
 
     @GetMapping("/workloads/{workloadId}")
-    public ResponseEntity<?> getWorkload(@PathVariable String workloadId, @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") Date startTime, @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") Date endTime) {
+    @Operation(description = "Get data and metadata about a specific workload")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Specific workload information", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Workload.class))),
+            @ApiResponse(responseCode = "400", description = "Malformed request", content = @Content),
+            @ApiResponse(responseCode = "500", description = "An error occurred on the server side", content = @Content)
+    })
+    public ResponseEntity<?> getWorkload(
+            @PathVariable @Parameter(description = "Unique workload ID") String workloadId,
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") @Parameter(description = "Date indicating the beginning of the aggregation") Date startTime,
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") @Parameter(description = "Date indicating the end of the aggregation") Date endTime
+    ) {
         return this.stubService.getWorkload(workloadId, startTime, endTime);
     }
 
     @GetMapping("/nodes")
-    public ResponseEntity<?> getNodes(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") Date startTime, @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") Date endTime) {
+    @Operation(description = "Get data and metadata about all worker nodes")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Nodes list", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Node.class))),
+            @ApiResponse(responseCode = "400", description = "Malformed request", content = @Content),
+            @ApiResponse(responseCode = "500", description = "An error occurred on the server side", content = @Content)
+    })
+    public ResponseEntity<?> getNodes(
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") @Parameter(description = "Date indicating the beginning of the aggregation") Date startTime,
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") @Parameter(description = "Date indicating the end of the aggregation") Date endTime
+    ) {
         return this.stubService.getNodes(startTime, endTime);
     }
 
     @GetMapping("/nodes/{nodeId}")
-    public ResponseEntity<?> getNode(@PathVariable String nodeId, @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") Date startTime, @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") Date endTime) {
+    @Operation(description = "Get data and metadata about a specific worker node")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Specific worker node information", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Node.class))),
+            @ApiResponse(responseCode = "400", description = "Malformed request", content = @Content),
+            @ApiResponse(responseCode = "500", description = "An error occurred on the server side", content = @Content)
+    })
+    public ResponseEntity<?> getNode(
+            @PathVariable @Parameter(description = "Unique worker node ID") String nodeId,
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") @Parameter(description = "Date indicating the beginning of the aggregation") Date startTime,
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") @Parameter(description = "Date indicating the end of the aggregation") Date endTime
+    ) {
         return this.stubService.getNode(nodeId, startTime, endTime);
     }
 
